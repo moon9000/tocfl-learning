f1d0955c02207740f1a1d0b9a3ba1252ea2b8892
Sabri <sabri.abdennader@wandi.fr>
2021-07-10 15:13:03 +0200

    feat(App.js): add NavBar component and bootstrap css dependencyon top of each component being displayed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router } from "react-router-dom";
+import { NavBar } from "./components/NavBar";
 import { Main } from "./components/Main";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 function App() {
   return (
     <Router>
     <div className="App">
+      <NavBar />
       <Main />
     </div>
   </Router>
