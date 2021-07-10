4a6c00aac488d5ce4aa7b608d570d953fa9441f6
Sabri <sabri.abdennader@wandi.fr>
2021-07-10 13:46:27 +0200

    feat(pages): add HomePage page with index

---
+++ src/pages/Home/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+export function HomePage() {
+  return (
+    <Router>
+      <div className="Home">
+            <h1>Welcome to TOCFL Learning</h1>
+            <h2>You will find here multiple tools to help you learning traditional chinese and study for the TOCFL exam</h2>
+      </div>
+    </Router>
+  );
+}
