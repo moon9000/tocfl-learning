2abf98bcb4e9294afad21d37b092a45ee296b3df
Sabri <sabri.abdennader@wandi.fr>
2021-07-10 15:23:09 +0200

    feat(components/Main): add route for ln parser

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { HomePage } from "../../pages/Home/HomePage";
+import { LnPage } from "../../pages/LN/LnPage";
 
 export function Main() {
   return (
     <Switch>
       {/* The Switch decides which component to show based on the current URL.*/}
       <Route exact path="/" component={HomePage}></Route>
+      <Route exact path="/ln" component={LnPage}></Route>
     </Switch>
   );
 }
