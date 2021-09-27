ad0b4f4363bcf7a2bcd083b4375a3ea72040fdd1
Sabri <sabri.abdennader@wandi.fr>
2021-09-27 23:01:22 +0200

    feat(server.js): add routes for words and animes db

--- src/server.js
+++ src/server.js
@@ -1,30 +1,57 @@
 const express = require("express");
-const upload = require("express-fileupload")
-
+var cors = require("cors");
+const upload = require("express-fileupload");
+const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
+const { JsonWebTokenError } = require("jsonwebtoken");
+
 const app = express();
+
+const db = require("./db/models");
+db.sequelize.sync();
+
+// // drop the table if it already exists
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+/*
+var corsOptions = {
+  origin: "http://localhost:5000",
+};
+*/
+app.use(cors());
 
-app.use(upload())
+// parse requests of content-type - application/json
+app.use(express.json()); /* bodyParser.json() is deprecated */
 
-app.post('/ln', (req, res) => {
-    if (req.files === null) {
-      return res.status(400).json({ msg: 'No file uploaded' });
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(
+  express.urlencoded({ extended: true })
+); /* bodyParser.urlencoded() is deprecated */
+
+app.use(upload());
+
+app.post("/ln", (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: "No file uploaded" });
+  }
+
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/pages/LN/data/${file.name}`, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
     }
-  
-    const file = req.files.file;
 
-    file.mv(`${__dirname}/pages/LN/data/${file.name}`, err => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send(err);
-      }
-  
-      res.json({ fileName: file.name, filePath: `data/${file.name}` });
-    });
+    res.json({ fileName: file.name, filePath: `data/${file.name}` });
   });
+});
+
+require("./db/routes/anime.route")(app); //to use routes related to db models and controllers
+require("./db/routes/word.route")(app); //to use routes related to db models and controllers
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
