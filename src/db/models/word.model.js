d6ef0f79a4de01a90c9a42f141b1cf7915dabfa7
Sabri <sabri.abdennader@wandi.fr>
2021-09-27 23:00:45 +0200

    feat(db/models): fix word model by using timestamp false

--- src/db/models/word.model.js
+++ src/db/models/word.model.js
@@ -1,27 +1,31 @@
 module.exports = (sequelize, Sequelize) => {
-  const Word = sequelize.define("Word", {
-    chinese: {
-      type: Sequelize.STRING,
+  const Word = sequelize.define(
+    "Word",
+    {
+      chinese: {
+        type: Sequelize.STRING,
+      },
+      pinyin: {
+        type: Sequelize.STRING,
+      },
+      english: {
+        type: Sequelize.STRING,
+      },
+      type: {
+        type: Sequelize.STRING,
+      },
+      level: {
+        type: Sequelize.STRING,
+      },
+      sentence_ch: {
+        type: Sequelize.STRING,
+      },
+      sentence_eng: {
+        type: Sequelize.STRING,
+      },
     },
-    pinyin: {
-      type: Sequelize.STRING,
-    },
-    english: {
-      type: Sequelize.STRING,
-    },
-    type: {
-      type: Sequelize.STRING,
-    },
-    level: {
-      type: Sequelize.STRING,
-    },
-    sentence_ch: {
-      type: Sequelize.STRING,
-    },
-    sentence_eng: {
-      type: Sequelize.STRING,
-    },
-  });
+    { timestamps: false }
+  );
 
   return Word;
 };
