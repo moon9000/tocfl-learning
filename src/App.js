288b06cfbdb9fc31221414dbbac099bafb39350c
Sabri <sabri.abdennader@wandi.fr>
2022-10-22 17:27:33 +0200

    fix(src/App.js): wrap everything in a router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
 import { Main } from "./components/Main";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <Router>
-    <div className="App">
       <NavBar />
-      <Main />
-    </div>
-  </Router>
+      <div className="App">
+        <Main />
+      </div>
+    </Router>
   );
 }
 
