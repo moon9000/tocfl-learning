4bf603ea4245be87b962d36eb1861b0c67531945
moon9000 <sebros02@gmail.com>
2024-05-09 02:41:38 +0200

    remove pages

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,21 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { HomePage } from "../../pages/Home/HomePage";
-import { LnPage } from "../../pages/LN/LnPage";
 import { ListPage } from "../../pages/List/ListPage";
-import { TaiwanInfosPage } from "../../pages/TaiwanInfos/TaiwanInfosPage";
 
 export function Main() {
   return (
     <Switch>
       {/* The Switch decides which component to show based on the current URL.*/}
       <Route exact path="/" component={HomePage}></Route>
-      <Route exact path="/ln" component={LnPage}></Route>
-      <Route
-        exact
-        path="/taiwan-residency-infos"
-        component={TaiwanInfosPage}
-      ></Route>
       <Route exact path="/words" component={ListPage}></Route>
     </Switch>
   );
