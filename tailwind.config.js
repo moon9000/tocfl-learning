3f155920eecaf050aafab1b275007406f998d646
moon9000 <sebros02@gmail.com>
2024-05-10 23:03:45 +0200

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,32 @@
-const colors = require("tailwindcss/colors");
+/** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: 'selector',
+  content: ['./src/**/*.{html,js}'],
   theme: {
     colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      black: colors.black,
-      white: colors.white,
-      gray: colors.trueGray,
-      indigo: colors.indigo,
-      red: colors.rose,
-      yellow: colors.amber,
-      lightDark: '#303030'
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'lightDark': '#303030'
     },
-    extend: {},
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-};
+}
